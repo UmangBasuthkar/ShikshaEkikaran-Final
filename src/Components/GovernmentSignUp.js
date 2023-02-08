85ba6a60f42c7a79d7fd7029c386b944a96ed8e2
UmangBasuthkar <118044656+UmangBasuthkar@users.noreply.github.com>
2023-02-08 23:56:26 +0530

    Update GovernmentSignUp.js

--- src/Components/GovernmentSignUp.js
+++ src/Components/GovernmentSignUp.js
@@ -10,16 +10,16 @@ export const GovernmentSignUp = () => {
             <form style={{"border":"none"}}>
                 <label for = "firstName"><b>First name</b></label>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 <label for = "lastName"><b>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Last name</b></label><br/>
-                <input type="text" value = {firstName} onChange = {(e) => handleInputChange(e)} id = "firstName" />&emsp;
-                <input type="text" value = {lastName} onChange = {(e) => handleInputChange(e)} id = "lastName"/><br/><br/>
+                <input type="text"/>&emsp;
+                <input type="text"/><br/><br/>
                 <label for = "emaiil"><b>Email</b></label><br/>
-                <input type="text" style={{"width":"92%"}} value = {email} onChange = {(e) => handleInputChange(e)} id = "email" placeholder="example@gmail.com"/><br/><br/>
+                <input type="text" style={{"width":"92%"}} placeholder="example@gmail.com"/><br/><br/>
                 <label for = "aadharCard"><b>Enter Aadhar Card Number</b></label><br/>
-                <input type="text" style={{"width":"92%"}} value = {aadharCard} onChange = {(e) => handleInputChange(e)} id = "aadharCard"/><br/><br/>
+                <input type="text" style={{"width":"92%"}}/><br/><br/>
                 <label for = "password"><b>Password</b></label><br/>
-                <input type="password" style={{"width":"92%"}} value = {password} onChange = {(e) => handleInputChange(e)} id = "password" placeholder="Enter at least 8 characters"/><br/><br/>
+                <input type="password" style={{"width":"92%"}} placeholder="Enter at least 8 characters"/><br/><br/>
                 <a href = "Governmentview">
-                    <button onClick={() => handleSubmit()} type="submit">Sign Up</button>
+                    <button type="button">Sign Up</button>
                 </a>
             </form>
         </div>
